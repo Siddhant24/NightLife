5cd62a390e0df8543b316c107c64cdb126fca8c7
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-10 12:40:43 +0000

    boilerplate edit

--- app/models/users.js
+++ app/models/users.js
@@ -10,9 +10,7 @@ var User = new Schema({
 		username: String,
       publicRepos: Number
 	},
-   nbrClicks: {
-      clicks: Number
-   }
+	bars: []
 });
 
 module.exports = mongoose.model('User', User);
