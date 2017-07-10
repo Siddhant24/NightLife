5d32f1940737795e67537f0effbbf4497b9d9511
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-10 13:09:28 +0000

    removed click routes from boilerplate

--- app/routes/index.js
+++ app/routes/index.js
@@ -50,8 +50,4 @@ module.exports = function (app, passport) {
 			failureRedirect: '/login'
 		}));
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
